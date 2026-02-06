7105f88f9f9024a572826400908cc916e425fc63
fundamentalprinciple <princedixit931@gmail.com>
2026-02-06 09:28:07 +0000

    pandas data selection, conditional data selection and dropping

--- MLP/pandas3.py
+++ MLP/pandas3.py
@@ -3,6 +3,66 @@ from sklearn.datasets import load_diabetes
 
 diabetes = load_diabetes(as_frame=True)
 df = diabetes['data']
-print(df.head(n=442))
-print(df.tail(n=10))
- 
+print(df)
+
+'''
+#.loc, .iloc
+
+print(df.iloc[0:5])
+print(df.loc[0])
+
+#conditional selection
+print(df['age'] > 0)
+print(df[df['age'] > 0])
+print(df.loc[df['age'] > 0])
+
+print(df.loc[(df['age'] > 0) & (df['sex'] < 0)])
+print(df.loc[(df['age'] > 0) | (df['sex'] > 0)])
+
+print(df['age'].mean())
+
+
+print(df['age'][0])
+
+#df = df.set_index('age')
+df = df.set_index('age', drop=False)
+print(df)
+print(df.loc[0.038075906433423026])
+df = df.reset_index(drop=True)
+print(df)
+
+
+#df[df['age'] < 0]['sex'] = 0
+df.loc[df['age'] < 1, 'sex'] = 0
+print(df)
+
+
+subset = df[df['age'] < 0].copy()
+subset['sex'] = 0
+print(subset)
+
+
+print(df.mean())
+print(df.mean(axis=1))
+df['avg'] = df.mean(axis=1)
+print(df)
+'''
+
+
+print(df.drop('age', axis=1))
+print(df.drop(0, axis=0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
