0e6d75b8cdacb8256efbaf9d1fa8072dfe0d2191
fundamentalprinciple <princedixit931@gmail.com>
2026-02-09 09:56:17 +0000

    pytorch tensor manipulations

--- DL/pytorch2.py
+++ DL/pytorch2.py
@@ -1,11 +1,51 @@
 import torch
 import numpy as np
 
-t = torch.rand(3,4)
+'''
+t = torch.rand(300,400)
 print(t)
 print(t.shape)
-print(torch.min(t))
-print(torch.max(t))
-print(torch.mean(t))
-print(torch.std(t))
-print(f"{torch.min(t):.10f}")
+print((torch.min(t),torch.max(t),torch.mean(t),torch.std(t)))
+print(f"{torch.min(t):.6f}")
+'''
+
+#image-like tensors
+
+'''
+t = torch.rand(224,224,3)
+print(t)
+print(t.shape)
+print(t.dtype)
+print(t.element_size())
+print(t.nelement())
+print(t.nelement()*t.element_size())
+'''
+
+'''
+h, w, shape = t.shape
+print(h, w, shape)
+pixels = h*w
+print(pixels)
+'''
+
+'''
+t = torch.arange(0,100,2)
+print(t)
+
+t = torch.zeros_like(t)
+print(t)
+print(t.device)
+'''
+
+'''
+t = torch.tensor([3.0, 6.0, 9.0], requires_grad=False)
+'''
+
+t = torch.rand(2,4)
+s = torch.rand(4,2)
+print(t+10, t-10, t*10)
+print(t*t)
+print(torch.matmul(t,s))
+print(t@s)
+
+
