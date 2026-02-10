31a583955cadb86b84850dca443de1f6b082d37a
fundamentalprinciple <princedixit931@gmail.com>
2026-02-10 10:13:04 +0000

    tensor reshaping and indexing

--- DL/pytorch2.py
+++ DL/pytorch2.py
@@ -39,7 +39,6 @@ print(t.device)
 
 '''
 t = torch.tensor([3.0, 6.0, 9.0], requires_grad=False)
-'''
 
 t = torch.rand(2,4)
 s = torch.rand(4,2)
@@ -47,5 +46,87 @@ print(t+10, t-10, t*10)
 print(t*t)
 print(torch.matmul(t,s))
 print(t@s)
+'''
+
+'''
+t = torch.arange(1,10)
+print(t)
+print(t.shape)
+t = t.reshape(3,3)
+print(t)
+print(t.shape)
+'''
+
+'''
+t = torch.arange(1,10)
+v = t.view(3,3)
+print(v)
+'''
+
+'''
+t = torch.arange(1,10)
+t = t.reshape(3,3)
+s = torch.stack([t,t,t,t], dim=0)
+print(s)
+print(s.shape)
+'''
+
+'''
+t = torch.arange(1,8).reshape(1,7)
+print(t)
+print(t.shape)
+t = t.squeeze()
+print(t)
+print(t.shape)
+t = t.unsqueeze(0).unsqueeze(0)
+print(t)
+print(t.shape)
+'''
+
+t = torch.arange(1,10).reshape(3,3)
+print(t)
+t = t.permute(1,0)
+print(t)
+print(t.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
