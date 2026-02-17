069fd421ec03066866497cb969f8e28f68589718
fundamentalprinciple <princedixit931@gmail.com>
2026-02-17 08:21:10 +0000

    TensorDataset, DataLoader classes for preparing batches of data and run iterations (for a simple Linear Regression)

--- DL/LinearRegression/simplestLinearRegression.py
+++ DL/LinearRegression/simplestLinearRegression.py
@@ -1,9 +1,72 @@
 import torch
-x = torch.arange(0,1,0.02).unsqueeze(dim=1)
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+
+X = torch.arange(0,1,0.02).unsqueeze(dim=1)
 #print(x, x.shape)
 
 w = 0.7
 b = 0.3
 
-y = 0.7*x + b
-print(y)
+Y = 0.7*X + b
+#print(y)
+
+train_split = int(0.8*len(X))
+X_train = X[:train_split]
+Y_train = Y[:train_split]
+
+X_test = X[train_split:]
+Y_test = Y[train_split:]
+
+train_dataset = TensorDataset(X_train, Y_train)
+test_dataset = TensorDataset(X_test, Y_test)
+
+#print(X_train[6],Y_train[6])
+#print(train_dataset[6])
+
+batch_size = 8
+num_workers = 0
+
+'''
+A FAILED ATTEMPT TO ESTIMATE W WITH LINEAR ALGEBRA
+
+XXT_P = torch.linalg.pinv(torch.matmul(X_train,torch.transpose(X_train,0,1)))
+XXT_PX = torch.matmul(torch.transpose(X_train,0,1),XXT_P)
+print(XXT_PX.shape)
+W = torch.matmul(XXT_PX, Y_train)
+print(W,W.shape)
+'''
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size = batch_size,
+    shuffle = True,
+    num_workers = num_workers
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size = batch_size,
+    shuffle = False,
+    num_workers = num_workers
+)
+
+for batch_X, batch_Y in train_loader:
+    print(batch_X.shape)
+    print(batch_Y.shape)
+    break    
+
+#print(test_dataset[0:])
+
+for batch_X, batch_Y in test_loader:
+    print(batch_X.shape)
+    print(batch_Y.shape)
+    #print(batch_X)
+    #print(batch_Y)
+        
+
+
+
+
+
+
