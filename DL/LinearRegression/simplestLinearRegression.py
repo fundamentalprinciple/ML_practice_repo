9b7bc49439c6085a4afb521bce82545e98bfea68
fundamentalprinciple <princedixit931@gmail.com>
2026-02-18 08:00:48 +0000

    plotting train and test data, also predictions (that I don't have yet)

--- DL/LinearRegression/simplestLinearRegression.py
+++ DL/LinearRegression/simplestLinearRegression.py
@@ -2,6 +2,8 @@ import torch
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
 
+import matplotlib.pyplot as plt
+
 X = torch.arange(0,1,0.02).unsqueeze(dim=1)
 #print(x, x.shape)
 
@@ -52,18 +54,59 @@ test_loader = DataLoader(
 )
 
 for batch_X, batch_Y in train_loader:
-    print(batch_X.shape)
-    print(batch_Y.shape)
+    #print(batch_X.shape)
+    #print(batch_Y.shape)
     break    
 
 #print(test_dataset[0:])
 
 for batch_X, batch_Y in test_loader:
-    print(batch_X.shape)
-    print(batch_Y.shape)
+    #print(batch_X.shape)
+    #print(batch_Y.shape)
     #print(batch_X)
     #print(batch_Y)
+    break
         
+def plot_predictions(
+    train_data=X_train,
+    train_labels=Y_train,
+    test_data=X_test,
+    test_labels=Y_test,
+    predictions=None
+):
+    plt.figure(figsize=(10,7))
+    plt.scatter(train_data, train_labels,
+                c="green",
+                s=25,
+                alpha=0.8,
+                label="Training Data",
+                marker="o")    
+
+    plt.scatter(test_data, test_labels,
+                c="blue",
+                s=25,
+                alpha=0.8,
+                label="Test Data",
+                marker="^")
+
+    if predictions is not None:
+        plt.scatter(test_data, predictions,
+                    c="red",
+                    s=25,
+                    alpha=0.8,
+                    label="Predictions",
+                    marker="X")
+
+    plt.xlabel("X values", fontsize=14)
+    plt.ylabel("Y values", fontsize=14)
+    plt.title("Linear Regression", fontsize=16, fontweight="bold")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+    plt.show()
+    return
+
+plot_predictions()
 
 
 
